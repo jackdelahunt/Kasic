8ba061f56ed1c736a3a5b6a2166e891626645402
jackdelahunt <jdelahun@redhat.com>
2021-07-21 02:47:14 +0100

    Tests: cleaned yp test for easier reuse

--- E2E/Tests/E2ETests.cs
+++ E2E/Tests/E2ETests.cs
@@ -6,43 +6,17 @@ namespace E2E.Tests
 {
     public class E2ETests
     {
-        private static TestData[] data = new[]
+        [TestCase("add 10 10", "20")]
+        [TestCase("num 10 | add -123 | string", "-113")]
+        [TestCase("string 20d | replace d 0 | num | add 200", "400")]
+        public void SingleLineTest(string commandInput, string output)
         {
-            new TestData
+            var result = Program.RunLine(commandInput);
+            if (result.Error != null)
             {
-                CommandInput = "add 10 10",
-                ExpectedResult = "20"
-            },
-            new TestData
-            {
-                CommandInput = "num 10 | add -123 | string",
-                ExpectedResult = "-113"
-            },
-            new TestData
-            {
-                CommandInput = "string 20d | replace d 0 | num | add 200",
-                ExpectedResult = "400"
-            },
-        };
-
-        [Test]
-        public void SingleLineTest()
-        {
-            foreach (var iter in data)
-            {
-                var result = Program.RunLine(iter.CommandInput);
-                if (result.Error != null)
-                {
-                    Assert.IsNull(result.Error, $"Input: {iter.CommandInput}");
-                }
-                Assert.AreEqual(iter.ExpectedResult, result.Value);
+                Assert.IsNull(result.Error, $"Input: {commandInput}");
             }
+            Assert.AreEqual(output, result.Value);
         }
-    }
-
-    public struct TestData
-    {
-        public string CommandInput;
-        public string ExpectedResult;
     }
 }
