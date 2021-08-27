2ef411cd3b1a6bea073e5ec57d6a19efcad6b66b
jackdelahunt <jdelahun@redhat.com>
2021-08-28 00:00:24 +0100

    Feat: added optional print to in

--- kasic/Commands/In.cs
+++ kasic/Commands/In.cs
@@ -15,14 +15,34 @@ namespace kasic.Commands
             CommandSettings = new CommandSettings()
             {
                 MinArgs = 0,
-                MaxArgs = 0,
-                ArgumentList = new ArgumentList(new List<KasicType>()),
+                MaxArgs = 1,
+                ArgumentList = new ArgumentList(new List<KasicType>()
+                {
+                    KasicType.ANY,
+                }),
                 ReturnType = KasicType.STRING,
             };
         }
 
         public override Result<IReturnObject, KasicError> Run(Context context, Arguments arguments, List<string> flags)
         {
+            if (arguments.Count > 0)
+            {
+                var annotationResult = arguments.AsAny(context, 0);
+                if (annotationResult.IsError)
+                {
+                    return Helpers.Error(annotationResult.Error);
+                }
+
+                if (flags.Contains("-l"))
+                {
+                    Logger.Log(annotationResult.Value.ToString());
+                }
+                else
+                {
+                    Logger.Logln(annotationResult.Value.ToString());
+                }
+            }
             var input = Console.ReadLine();
             if (input is null)
                 return new ReturnObject(this, "");
